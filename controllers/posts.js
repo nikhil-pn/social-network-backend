5fe74a84596afe7c88662fae2b6b7160e2b23b77
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-21 18:36:24 +0530

    deletePost created

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,5 @@
 const cloudinary = require("../middleware/cloudinary")
-
+const Post = require("../models/Post");
 module.exports = {
     createPost : async(req, res)=>{
         try {
@@ -11,6 +11,14 @@ module.exports = {
             })
         } catch (error) {
             console.log(error)
+        }
+    },
+    deletePost : async(req, res)=>{
+        try {
+            let post =  await Post.findById(req.params.id)
+            await cloudinary.uploader.destroy(post.cloudinaryId)
+        } catch (error) {
+            
         }
     }
 }
