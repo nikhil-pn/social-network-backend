909d99bb3836fa11167cb956ccc3836cba724eb3
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-21 13:59:52 +0530

    first commit server.js created

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+
+require("dotenv").config({ path: "./config/.env" });
+
+connectDB();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(logger("dev"))
+
+app.listen(process.env.PORT, ()=>{
+    console.log("Server is running");
+})
