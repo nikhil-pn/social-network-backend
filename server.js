0a120643f693efcb92a7126d695b3c3e8e2921a3
nikhilpn725 <nikhilpn725@gmail.com>
2023-01-21 15:52:54 +0530

    added express session and store session in monogdb

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const logger = require("morgan");
 const connectDB = require("./config/database");
 
 require("dotenv").config({ path: "./config/.env" });
+require("./config/passport")(passport);
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 connectDB();
 app.set("view engine", "ejs");
@@ -14,6 +17,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(logger("dev"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running");
